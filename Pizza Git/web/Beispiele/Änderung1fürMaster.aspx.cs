583ca441399c51e5e87e6445b66e6073c61db08f
Baztey <seb.schrei@web.de>
2014-01-18 23:27:18 +0100

    Erste Änderungen
    Website erstellt

---
+++ Pizza Git/web/Beispiele/Änderung1fürMaster.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace web.Beispiele
+{
+    public partial class Änderung1fürMaster : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
